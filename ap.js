43dd0f846cb700bf2c816e37f988fb0896e0feb2
= <=>
2020-12-01 21:01:47 +0600

    multipal json

--- ap.js
+++ ap.js
@@ -50,6 +50,10 @@ function loadData(e) {
 -----------------------------------------------*/
 
 document.getElementById('button1').addEventListener('click', loadCustomer);
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+
+// Load single suctomer
 
 function loadCustomer(e)	{
 	const	xhr = new XMLHttpRequest();
@@ -58,7 +62,63 @@ function loadCustomer(e)	{
 
 	xhr.onload = function(){
 		if(this.status === 200){
+			// console.log(this.responseText);
 
+
+			const customer = JSON.parse(this.responseText);
+
+			const output = `
+			<ul>
+				<li> ID: ${customer.id}</li>
+				<li> Name: ${customer.name}</li>
+				<li> Company: ${customer.company}</li>
+				<li> phone: ${customer.phone}</li>
+			</ul>
+			
+			`;
+			document.getElementById('customer').innerHTML = output;
+		}
+	}
+}
+
+
+
+
+
+// Load Customers
+
+
+function loadCustomers(e)	{
+	const	xhr = new XMLHttpRequest();
+
+	xhr.open('GET', 'customers.json', true);
+
+	xhr.onload = function(){
+		if(this.status === 200){
+			// console.log(this.responseText);
+
+
+			const customers = JSON.parse(this.responseText);
+
+			let output = ''; 
+
+			customers.forEach(function(customer){
+				output += `
+				<ul>
+					<li> ID: ${customers.id}</li>
+					<li> Name: ${customers.name}</li>
+					<li> Company: ${customers.company}</li>
+					<li> phone: ${customers.phone}</li>
+				</ul>
+				
+				`;
+
+			});
+
+
+
+			
+			document.getElementById('customer').innerHTML = output;
 		}
 	}
 }
